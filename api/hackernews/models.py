92acbd8dfe5dede1f28f21a36209516c10222a35
prgya <pragya8396@gmail.com>
2019-09-12 18:15:12 +0000

    Update models

--- api/hackernews/models.py
+++ api/hackernews/models.py
@@ -3,17 +3,16 @@ from django.db import models
 # Create your models here.
 
 class hackernews(models.Model):
-
-	story_id = models.CharField(max_length=20, primary_key=True)
-	username = models.CharField(max_length=40)
-	title = models.TextField()
-	sentiment = models.CharField(max_length=20)
-	url = models.TextField(max_length=100)
-	upvotes = models.IntegerField()
+    story_id = models.CharField(max_length=20, primary_key=True)
+    username = models.CharField(max_length=40)
+    title = models.TextField()
+    sentiment = models.CharField(max_length=20)
+    url = models.TextField(max_length=1000)
+    upvotes = models.IntegerField()
 	
 	
-	class Meta:
-		db_table = "app_hackernews"
+    class Meta:
+        db_table = "app_hackernews"
 	
-	# def __str__(self):
-	# 	return '{}-{}-{} {} {} {}'.format(self.story_id,self.username,self.title,self.sentiment,self.url,self.upvotes)
+    def __str__(self):
+        return '{} {} {} {} {} {}'.format(self.story_id,self.username,self.title,self.sentiment,self.url,self.upvotes)
